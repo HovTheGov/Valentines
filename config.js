4a2fec2942083f9f57617799b78e4a6ad40e320d
HovTheGov <hoviggames@gmail.com>
2025-02-13 22:09:53 -0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.youtube.com/watch?v=s82cxuM2IAI", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dqayigw48/video/upload/v1739513350/Arkadi_Dumikyan_-_IRAKAN_ES_elx7gx.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
